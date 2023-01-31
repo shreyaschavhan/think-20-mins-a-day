4d06a7785923748e9c68d1c7bbdadac0196351bc
Shreyas Chavhan <shreyaschavhan4@gmail.com>
2023-01-31 21:34:13 +0530

    `Day 022 - 31st Jan 2022`

---
+++ CodeChef/Day 022 - 31st Jan 2022/ZOOZ.py
@@ -0,0 +1,12 @@
+# cook your dish here
+
+
+for _ in range(int(input())):
+    N = int(input())
+    print('1', end='')
+    for i in range(N-2):
+        print('0', end='')
+    print('1')
+
+
+    

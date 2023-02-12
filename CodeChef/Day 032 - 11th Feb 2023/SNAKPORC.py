a0f7f9007620a405a7172a08d0763edc8b2d8c0d
Shreyas Chavhan <shreyaschavhan4@gmail.com>
2023-02-12 07:44:56 +0530

    `Day 032 - 11th Feb 2023`

---
+++ CodeChef/Day 032 - 11th Feb 2023/SNAKPORC.py
@@ -0,0 +1,18 @@
+```
+# cook your dish here
+
+for _ in range(int(input())):
+    __ = int(input())
+    report = input().replace('.', '').strip()
+    if len(report) == 0:
+        print('Valid')
+    elif len(report) % 2 == 0 and report[0] == 'H':
+        if 'HH' in report or 'TT' in report:
+            print('Invalid')
+        else:
+            print('Valid')
+    else:
+        print('Invalid')
+
+
+```
